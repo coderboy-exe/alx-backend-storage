b302badd5c1383a748ec8914f3f4334123babff5
Uchechukwu Ottah <usottah@gmail.com>
2023-04-07 04:43:18 +0000

    Update procedure

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -10,7 +10,7 @@ BEGIN
     UPDATE users
     SET average_score = (
 	SELECT AVG(score) FROM corrections
-	WHERE corrections.user_id = user_id;
+	WHERE corrections.user_id = user_id)
     WHERE id = user_id;
 END//
 
