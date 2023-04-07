6221632a8b84b370b1c3ac0c5a9e6a185d7487ac
Uchechukwu Ottah <usottah@gmail.com>
2023-04-07 04:10:06 +0000

    Update: changed date-getter method

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -2,7 +2,7 @@
 -- that have a score under 80 (strict) 
 -- and no last_meeting or more than 1 month
 
+DROP VIEW IF EXISTS need_meeting;
 CREATE VIEW need_meeting AS SELECT name
 FROM students WHERE score < 80
-AND (last_meeting IS NULL OR last_meeting < DATE(NOW(), INTERVAL 1 MONTH)
-);
+AND (last_meeting IS NULL OR last_meeting < DATE_SUB(NOW(), INTERVAL 1 MONTH));
