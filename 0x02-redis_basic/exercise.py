64a53df0ac9e24b0aac01349ab0d58688dbb3de6
Uchechukwu Ottah <usottah@gmail.com>
2023-04-13 04:56:15 +0000

    Updates: custom getter and parametrization functions - custom decorator to count number of Cache store calls (with the help of functools.wrap)

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,8 +2,21 @@
 """ Module definition """
 
 import redis
-from typing import Union
+from typing import Union, Optional, Callable
 import uuid
+from functools import wraps
+
+
+def count_calls(method: Callable) -> Callable:
+    """counts houw many times methods of the Cache class are called"""
+    key = method.__qualname__
+
+    @wraps(method)
+    def wrapper(self, *args, **kwds):
+        """ wrapper for decorator function """
+        self._redis.incr(key)
+        return method(self, *args, **kwds)
+    return wrapper
 
 
 class Cache:
@@ -13,8 +26,25 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """stores input data using a random key and returns key"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """converts key (if exists) from byte-string back to format"""
+        data = self._redis.get(key)
+        if data is not None and fn is not None:
+            data = fn(data)
+        return data
+
+    def get_str(self, key: str) -> Union[str, None]:
+        """ parametrize stored data(key) to string """
+        return self.get(key, str)
+
+    def get_int(self, key: int) -> Union[int, None]:
+        """ parametrize stored data(key) to int """
+        return self.get(key, int)
